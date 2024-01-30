66304978bd3f61f9648cfc476d7dcf0e11afcda9
João Nunes <jd.mn@hotmail.com>
2024-01-30 16:59:36 -0300

    Simulações dos pesos e análise da assertividade (comparativa)

--- reportsWeightsAnalysis.py
+++ reportsWeightsAnalysis.py
@@ -9,71 +9,94 @@ import csv
 
 # inicializações
 campos = ["Reputação", "Periodicidade", "Cobertura", "Escopo", "Abrangência", "Metodologia", "Peso Final", "Score"]
-pesosCampos = [1,0.5,0.5,0.5,0.5,0.9]
+camposWeights = ["p1", "p2", "p3", "p4", "p5", "p6", "acertividade"]
+#pesosCampos = [1,0.6,0.2,0.2,0.1,1]
+#pesosCampos = [2.2,0.8,0.6,0.6,0.8,2.2]
 compArray = []
 
-with open('scoresReports.csv', 'w', newline='') as s, open('pesosReports.csv', 'w', newline='') as p, open('entradaEmpresas.csv') as entrada, open('comparacaoAvaliacoes.csv', 'w', newline='') as c:
-	
-	csv_reader = csv.reader(entrada, delimiter=',')
+#np.arange(0, 1, 0.5)
+
+# Gera pesos aleatórios entre 1 e 10 (possivelmente vamos reduzir a escala futuramente)
+def generatorRandomWeights():
+	aux = []
+	for a in np.arange(0, 1, 0.1):
+		for b in np.arange(0, 1, 0.1):
+			for c in np.arange(0, 1, 0.1):
+				for d in np.arange(0, 1, 0.1):
+					for e in np.arange(0, 1, 0.1):
+						for f in np.arange(0, 1, 0.1):
+							aux.append([a,b,c,d,e,f])
+	return aux
+
+
+# Função principal
+# abre os arquivos de entrada, realiza testes com os pesos e salva num arquivo
+with open('scoresReports.csv', 'w', newline='') as s, open('pesosReports.csv', 'w', newline='') as p, open('comparacaoAvaliacoes.csv', 'w', newline='') as c:
 
 	scores = csv.writer(s)
 	scores.writerow(campos)
 	
 	pesos = csv.writer(p)
+	pesos.writerow(camposWeights)
 	comp = csv.writer(c)
 
-	somaWeights = 0
-	for item in pesosCampos :
-		somaWeights += item
-	
-	pesos.writerow(pesosCampos)
+	weights = generatorRandomWeights()
+	#print(weights)
+	for pesosCampos in weights:
+
+		somaWeights = 0
+		for item in pesosCampos :
+			somaWeights += item
 		
-	count = 0
-	equal = 0
-	diff = 0
-	for row in csv_reader:
-		if count != 0 :
-			reputacao = int(row[0])
-			periodicidade = int(row[1])
-			cobertura = int(row[2])
-			escopo = int(row[3])
-			abrangencia = int(row[4])
-			metodologia = int(row[5])
 			
-			company = [reputacao, periodicidade, cobertura, escopo, abrangencia, metodologia]
-			
-			pesoFinal = ((reputacao*pesosCampos[0])  + (periodicidade*pesosCampos[1]) + (cobertura*pesosCampos[2]) + (escopo*pesosCampos[3]) + (abrangencia*pesosCampos[4]) + (metodologia*pesosCampos[5]))/(somaWeights)*3.0
-			
-			if pesoFinal >= 5 :
-				rating = "Ótimo"
-			elif pesoFinal >= 3 :
-				rating = "Bom"
-			elif pesoFinal < 1 :
-				pesoFinal = 1.0
-				rating = "Ruim"
-			else :
-				rating = "Ruim"
+		count = 0
+		equal = 0
+		diff = 0
+		
+		with open('entradaEmpresas.csv') as entrada:
+
+			csv_reader = csv.reader(entrada, delimiter=',')
+
+			for linha in csv_reader:
+				count += 1
+
+				reputacao = int(linha[0])
+				periodicidade = int(linha[1])
+				cobertura = int(linha[2])
+				escopo = int(linha[3])
+				abrangencia = int(linha[4])
+				metodologia = int(linha[5])
 				
-			company.append(round(pesoFinal, 2))
-			company.append(rating)
+				company = [reputacao, periodicidade, cobertura, escopo, abrangencia, metodologia]
+				
+				pesoFinal = ((reputacao*pesosCampos[0])  + (periodicidade*pesosCampos[1]) + (cobertura*pesosCampos[2]) + (escopo*pesosCampos[3]) + (abrangencia*pesosCampos[4]) + (metodologia*pesosCampos[5]))/(somaWeights)*3.0
+				print(pesoFinal)
+				
+				if pesoFinal >= 5 :
+					rating = "Ótimo" # Muito Relevante
+				elif pesoFinal >= 3 :
+					rating = "Bom" # Relevante
+				else :
+					rating = "Ruim" # Pouco Relevante
+					
+				company.append(round(pesoFinal, 2))
+				company.append(rating)
 
-			compArray = [row[6], row[7], company[6], company[7]]
-			if compArray[1] == compArray[3]:
-				equal += 1
-			
-			if float(compArray[0]) < compArray[2]:
-				compArray.append("+")
-				diff += 1
-			elif float(compArray[0]) > compArray[2]:
-				compArray.append("-")
-			else :
-				compArray.append("=")
+				#comparando o gerad
+				compArray = [linha[6], linha[7], company[6], company[7], linha[8]]
+				if compArray[3] == compArray[4]:
+					equal += 1
 
-			comp.writerow(compArray)
-			scores.writerow(company)
-		
-		count += 1
+				comp.writerow(compArray)
+				scores.writerow(company)
+				
+			nota = [equal/count*100]
+			conf1 = conf2 = False
+			if pesosCampos[2] > pesosCampos[1] and pesosCampos[2] > pesosCampos[3] and pesosCampos[2] > pesosCampos[4]:
+				conf1 = True
+			if pesosCampos[2] < pesosCampos[0] and pesosCampos[2] < pesosCampos[5]:
+				conf2 = True
+			if nota[0] > 10.0 and conf1 and conf2:				
+				pesos.writerow(pesosCampos+nota)
 
-	print(count)
-	print(equal)
-	print(equal/count*100)		
+
