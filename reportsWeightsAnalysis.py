e0988b7e79e5361a5ae96b0fec8eaa6a2189f379
João Nunes <jd.mn@hotmail.com>
2024-01-31 18:24:54 -0300

    Separação em arquivos diferentes, dada a função de cada um

--- reportsWeightsAnalysis.py
+++ reportsWeightsAnalysis.py
@@ -9,26 +9,22 @@ import csv
 
 # inicializações
 campos = ["Reputação", "Periodicidade", "Cobertura", "Escopo", "Abrangência", "Metodologia", "Peso Final", "Score"]
-camposWeights = ["p1", "p2", "p3", "p4", "p5", "p6", "assertividade"]
-#pesosCampos = [1,0.6,0.2,0.2,0.1,1]
-#pesosCampos = [2.2,0.8,0.6,0.6,0.8,2.2]
+camposWeights = ["p1", "p2", "p3", "p4", "p5", "p6", "precisão"]
 compArray = []
 
-#np.arange(0, 1, 0.5)
-
-# Gera pesos aleatórios entre 1 e 10 (possivelmente vamos reduzir a escala futuramente)
+# Gera pesos aleatórios entre 1 e 10
 def generatorRandomWeights():
 	aux = []
-	for rep in np.arange(0.1, 1, 0.1):
-		for per in np.arange(0.1, 1, 0.1):
-			for cob in np.arange(0.1, 1, 0.1):
-				for esc in np.arange(0.1, 1, 0.1):
-					for abr in np.arange(0.1, 1, 0.1):
-						for met in np.arange(0.1, 1, 0.1):
+	for rep in np.arange(1, 11, 1):
+		for per in np.arange(1, 11, 1):
+			for cob in np.arange(1, 11, 1):
+				for esc in np.arange(1, 11, 1):
+					for abr in np.arange(1, 11, 1):
+						for met in np.arange(1, 11, 1):
 							aux.append([rep,per,cob,esc,abr,met])
 	return aux
 
-
+# ===========================================================================
 # Função principal
 # abre os arquivos de entrada, realiza testes com os pesos e salva num arquivo
 with open('scoresReports.csv', 'w', newline='') as s, open('pesosReports.csv', 'w', newline='') as p, open('comparacaoAvaliacoes.csv', 'w', newline='') as c:
@@ -41,7 +37,6 @@ with open('scoresReports.csv', 'w', newline='') as s, open('pesosReports.csv', '
 	comp = csv.writer(c)
 
 	weights = generatorRandomWeights()
-	#print(weights)
 	for pesosCampos in weights:
 
 		somaWeights = 0
@@ -51,7 +46,6 @@ with open('scoresReports.csv', 'w', newline='') as s, open('pesosReports.csv', '
 			
 		count = 0
 		equal = 0
-		diff = 0
 		
 		with open('entradaEmpresas.csv') as entrada:
 
@@ -96,7 +90,5 @@ with open('scoresReports.csv', 'w', newline='') as s, open('pesosReports.csv', '
 				conf1 = True
 			if pesosCampos[2] < pesosCampos[0] and pesosCampos[2] < pesosCampos[5]:
 				conf2 = True
-			if nota[0] > 80.0 and conf1 and conf2:				
+			if nota[0] > 10.0 and conf1 and conf2:				
 				pesos.writerow(pesosCampos+nota)
-
-
