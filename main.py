cda1e0d4a48a29d2fd814f6fd1805c003afa9dfa
João Nunes <jd.mn@hotmail.com>
2024-03-12 17:42:02 -0300

    Início da criação do Banco de Dados usando SQLite3

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import statistics
+import math
+import random
+import sqlite3
+import bancosqlite
+
+
+bancosqlite.cria_tabela()
+ 
