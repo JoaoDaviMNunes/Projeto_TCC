c3dc9a6a17dd425b986272e901350a2bf1bf9ddd
João Nunes <jd.mn@hotmail.com>
2024-06-12 15:33:43 -0300

    Ajustes nas variáveis de armazenamento da Avaliação

--- TCC/analises.py
+++ TCC/analises.py
@@ -11,9 +11,7 @@ def main():
     dado_lst = []
 
     # contador do número de requisições que possuem cada um dos tipos de ataque
-    numM_min, numP_min, numD_min = 0,0,0
-    numM_media, numP_media, numD_media = 0,0,0
-    numM_max, numP_max, numD_max = 0,0,0
+    numM, numP, numD = 0,0,0
     # acumulador de custos de cada tipo de ataque
     malware_baixo_custos, phishing_baixo_custos, ddos_baixo_custos = 0,0,0
     malware_meio_custos, phishing_meio_custos, ddos_meio_custos = 0,0,0
@@ -30,9 +28,7 @@ def main():
     ddos_alto_min,ddos_alto_media,ddos_alto_max = 0,0,0
 
     # contador do número de requisições que possuem cada um dos setores
-    numF_min, numC_min, numS_min = 0,0,0
-    numF_media, numC_media, numS_media = 0,0,0
-    numF_max, numC_max, numS_max = 0,0,0
+    numF, numC, numS = 0,0,0
     # acumulador de custos de cada setor
     financeiro_baixo_custos, comercio_baixo_custos, saude_baixo_custos = 0,0,0      
     financeiro_meio_custos, comercio_meio_custos, saude_meio_custos = 0,0,0
@@ -61,13 +57,12 @@ def main():
         numReq = 0
         empresas, valores = [],[]
 
-        # -------------------------------------------------------------
+        # COLETA DE DADOS
         # info[0] = infos requisição
         # info[1] = infos malware
         # info[2] = infos phishing
         # info[3] = infos ddos
         # info[4] = infos custos
-        # COLETA DE DADOS
         for info in dado_lst:
             #print(info)
             numReq += 1
@@ -76,103 +71,261 @@ def main():
             empresas.append(numEmp)
             valores.append(info[4][0])
 
-            # pegando informações de malware
+            # pegando informações de malware --------------------------------
             if info[1][0] != 0 and info[0][3] == '1' and info[0][4] == '0' and info[0][5] == '0':
                 numM += 1
-                malware_custos += info[4][0]
-                if malware_min == 0 and malware_max == 0:
-                    malware_min, malware_max = info[4][1], info[4][2]
-                if info[4][1] < malware_min:
-                    malware_min = info[4][1]
-                if info[4][2] > malware_max:
-                    malware_max = info[4][2]
+                # baixo
+                malware_baixo_custos += info[4][1]
+                if malware_baixo_min == 0 and malware_baixo_max == 0:
+                    malware_baixo_min, malware_baixo_max = info[4][1], info[4][1]
+                elif info[4][1] < malware_baixo_min:
+                    malware_baixo_min = info[4][1]
+                elif info[4][1] > malware_baixo_max:
+                    malware_baixo_max = info[4][1]
+                # meio
+                malware_meio_custos += info[4][0]
+                if malware_meio_min == 0 and malware_meio_max == 0:
+                    malware_meio_min, malware_meio_max = info[4][0], info[4][0]
+                if info[4][0] < malware_meio_min:
+                    malware_meio_min = info[4][0]
+                if info[4][0] > malware_meio_max:
+                    malware_meio_max = info[4][0]
+                # alto
+                malware_alto_custos += info[4][2]
+                if malware_alto_min == 0 and malware_alto_max == 0:
+                    malware_alto_min, malware_alto_max = info[4][2], info[4][2]
+                if info[4][2] < malware_alto_min:
+                    malware_alto_min = info[4][2]
+                if info[4][2] > malware_alto_max:
+                    malware_alto_max = info[4][2]
 
-            # pegando informações de phishing
+            # pegando informações de phishing --------------------------------
             if info[2][0] != 0 and info[0][3] == '0' and info[0][4] == '1' and info[0][5] == '0':
                 numP += 1
-                phishing_custos += info[4][0]
-                if phishing_min == 0 and phishing_max == 0:
-                    phishing_min, phishing_max = info[4][1], info[4][2]
-                if info[4][1] < phishing_min:
-                    phishing_min = info[4][1]
-                if info[4][2] > phishing_max:
-                    phishing_max = info[4][2]
+                # baixo
+                phishing_baixo_custos += info[4][1]
+                if phishing_baixo_min == 0 and phishing_baixo_max == 0:
+                    phishing_baixo_min, phishing_baixo_max = info[4][1], info[4][1]
+                if info[4][1] < phishing_baixo_min:
+                    phishing_baixo_min = info[4][1]
+                if info[4][1] > phishing_baixo_max:
+                    phishing_baixo_max = info[4][1]
+                # meio
+                phishing_meio_custos += info[4][0]
+                if phishing_meio_min == 0 and phishing_meio_max == 0:
+                    phishing_meio_min, phishing_meio_max = info[4][0], info[4][0]
+                if info[4][0] < phishing_meio_min:
+                    phishing_meio_min = info[4][0]
+                if info[4][0] > phishing_meio_max:
+                    phishing_meio_max = info[4][0]
+                # alto
+                phishing_alto_custos += info[4][2]
+                if phishing_alto_min == 0 and phishing_alto_max == 0:
+                    phishing_alto_min, phishing_alto_max = info[4][2], info[4][2]
+                if info[4][2] < phishing_alto_min:
+                    phishing_alto_min = info[4][2]
+                if info[4][2] > phishing_alto_max:
+                    phishing_alto_max = info[4][2]
 
-            # pegando informações de DDoS
+            # pegando informações de DDoS --------------------------------
             if info[3][0] != 0 and info[0][3] == '0' and info[0][4] == '0' and info[0][5] == '1':
                 numD += 1
-                ddos_custos += info[4][0]
-                if ddos_min == 0 and ddos_max == 0:
-                    ddos_min, ddos_max = info[4][1], info[4][2]
-                if info[4][1] < ddos_min:
-                    ddos_min = info[4][1]
-                if info[4][2] > ddos_max:
-                    ddos_max = info[4][2]
+                # baixo
+                ddos_baixo_custos += info[4][1]
+                if ddos_baixo_min == 0 and ddos_baixo_max == 0:
+                    ddos_baixo_min, ddos_baixo_max = info[4][1], info[4][1]
+                if info[4][1] < ddos_baixo_min:
+                    ddos_baixo_min = info[4][1]
+                if info[4][1] > ddos_baixo_max:
+                    ddos_baixo_max = info[4][1]
+                # meio
+                ddos_meio_custos += info[4][0]
+                if ddos_meio_min == 0 and ddos_meio_max == 0:
+                    ddos_meio_min, ddos_meio_max = info[4][0], info[4][0]
+                if info[4][0] < ddos_meio_min:
+                    ddos_meio_min = info[4][0]
+                if info[4][0] > ddos_meio_max:
+                    ddos_meio_max = info[4][0]
+                # alto
+                ddos_alto_custos += info[4][2]
+                if ddos_alto_min == 0 and ddos_alto_max == 0:
+                    ddos_alto_min, ddos_alto_max = info[4][2], info[4][2]
+                if info[4][2] < ddos_alto_min:
+                    ddos_alto_min = info[4][2]
+                if info[4][2] > ddos_alto_max:
+                    ddos_alto_max = info[4][2]
 
-            # pegando informações do setor financeiro
+            # pegando informações do setor financeiro --------------------------------
             if info[0][0] == '1':
                 #print('Setor financeiro')
                 numF += 1
-                financeiro_custos += info[4][0]
-                if financeiro_min == 0 and financeiro_max == 0:
-                    financeiro_min, financeiro_max = info[4][1], info[4][2]
-                if info[4][1] < financeiro_min:
-                    financeiro_min = info[4][1]
-                if info[4][2] > financeiro_max:
-                    financeiro_max = info[4][2]
+                # baixo
+                financeiro_baixo_custos += info[4][1]
+                if financeiro_baixo_min == 0 and financeiro_baixo_max == 0:
+                    financeiro_baixo_min, financeiro_baixo_max = info[4][1], info[4][1]
+                if info[4][1] < financeiro_baixo_min:
+                    financeiro_baixo_min = info[4][1]
+                if info[4][1] > financeiro_baixo_max:
+                    financeiro_baixo_max = info[4][1]
+                # meio
+                financeiro_meio_custos += info[4][0]
+                if financeiro_meio_min == 0 and financeiro_meio_max == 0:
+                    financeiro_meio_min, financeiro_meio_max = info[4][0], info[4][0]
+                if info[4][0] < financeiro_meio_min:
+                    financeiro_meio_min = info[4][0]
+                if info[4][0] > financeiro_meio_max:
+                    financeiro_meio_max = info[4][0]
+                # alto
+                financeiro_alto_custos += info[4][2]
+                if financeiro_alto_min == 0 and financeiro_alto_max == 0:
+                    financeiro_alto_min, financeiro_alto_max = info[4][2], info[4][2]
+                if info[4][2] < financeiro_alto_min:
+                    financeiro_alto_min = info[4][2]
+                if info[4][2] > financeiro_alto_max:
+                    financeiro_alto_max = info[4][2]
+            # pegando informações do setor de comércio --------------------------------
             elif info[0][1] == '1':
                 #print('Setor de comércio')
                 numC += 1
-                comercio_custos += info[4][0]
-                if comercio_min == 0 and comercio_max == 0:
-                    comercio_min, comercio_max = info[4][1], info[4][2]
-                if info[4][1] < comercio_min:
-                    comercio_min = info[4][1]
-                if info[4][2] > comercio_max:
-                    comercio_max = info[4][2]
+                # baixo
+                comercio_baixo_custos += info[4][1]
+                if comercio_baixo_min == 0 and comercio_baixo_max == 0:
+                    comercio_baixo_min, comercio_baixo_max = info[4][1], info[4][1]
+                if info[4][1] < comercio_baixo_min:
+                    comercio_baixo_min = info[4][1]
+                if info[4][1] > comercio_baixo_max:
+                    comercio_baixo_max = info[4][1]
+                # meio
+                comercio_meio_custos += info[4][0]
+                if comercio_meio_min == 0 and comercio_meio_max == 0:
+                    comercio_meio_min, comercio_meio_max = info[4][0], info[4][0]
+                if info[4][0] < comercio_meio_min:
+                    comercio_meio_min = info[4][0]
+                if info[4][0] > comercio_meio_max:
+                    comercio_meio_max = info[4][0]
+                # alto
+                comercio_alto_custos += info[4][2]
+                if comercio_alto_min == 0 and comercio_alto_max == 0:
+                    comercio_alto_min, comercio_alto_max = info[4][2], info[4][2]
+                if info[4][2] < comercio_alto_min:
+                    comercio_alto_min = info[4][2]
+                if info[4][2] > comercio_alto_max:
+                    comercio_alto_max = info[4][2]
+            # pegando informações do setor de saúde --------------------------------
             elif info[0][2] == '1':
                 #print('Setor de saúde')
                 numS += 1
-                saude_custos += info[4][0]
-                if saude_min == 0 and saude_max == 0:
-                    saude_min, saude_max = info[4][1], info[4][2]
-                if info[4][1] < saude_min:
-                    saude_min = info[4][1]
-                if info[4][2] > saude_max:
-                    saude_max = info[4][2]
+                # baixo
+                saude_baixo_custos += info[4][1]
+                if saude_baixo_min == 0 and saude_baixo_max == 0:
+                    saude_baixo_min, saude_baixo_max = info[4][1], info[4][1]
+                if info[4][1] < saude_baixo_min:
+                    saude_baixo_min = info[4][1]
+                if info[4][1] > saude_baixo_max:
+                    saude_baixo_max = info[4][1]
+                # meio
+                saude_meio_custos += info[4][0]
+                if saude_meio_min == 0 and saude_meio_max == 0:
+                    saude_meio_min, saude_meio_max = info[4][0], info[4][0]
+                if info[4][0] < saude_meio_min:
+                    saude_meio_min = info[4][0]
+                if info[4][0] > saude_meio_max:
+                    saude_meio_max = info[4][0]
+                # alto
+                saude_alto_custos += info[4][2]
+                if saude_alto_min == 0 and saude_alto_max == 0:
+                    saude_alto_min, saude_alto_max = info[4][2], info[4][2]
+                if info[4][2] < saude_alto_min:
+                    saude_alto_min = info[4][2]
+                if info[4][2] > saude_alto_max:
+                    saude_alto_max = info[4][2]
         
     # -------------------------------------------------------------
     # CÁLCULO DAS MÉDIAS E COMPARAÇÃO DE VARIÁVEIS
     mediaCustos = round(somaTotais/numReq,2)
-    malware_media = round(malware_custos/numM,2)
-    phishing_media = round(phishing_custos/numP,2)
-    ddos_media = round(ddos_custos/numD,2)
-    financeiro_media = round(financeiro_custos/numF,2)
-    comercio_media = round(comercio_custos/numC,2)
-    saude_media = round(saude_custos/numS,2)
+
+    malware_baixo_media = round(malware_baixo_custos/numM,2)
+    malware_meio_media = round(malware_meio_custos/numM,2)
+    malware_alto_media = round(malware_alto_custos/numM,2)
+    phishing_baixo_media = round(phishing_baixo_custos/numP,2)
+    phishing_meio_media = round(phishing_meio_custos/numP,2)
+    phishing_alto_media = round(phishing_alto_custos/numP,2)
+    ddos_baixo_media = round(ddos_baixo_custos/numD,2)
+    ddos_meio_media = round(ddos_meio_custos/numD,2)
+    ddos_alto_media = round(ddos_alto_custos/numD,2)
+    
+    financeiro_baixo_media = round(financeiro_baixo_custos/numF,2)
+    financeiro_meio_media = round(financeiro_meio_custos/numF,2)
+    financeiro_alto_media = round(financeiro_alto_custos/numF,2)
+    comercio_baixo_media = round(comercio_baixo_custos/numC,2)
+    comercio_meio_media = round(comercio_meio_custos/numC,2)
+    comercio_alto_media = round(comercio_alto_custos/numC,2)
+    saude_baixo_media = round(saude_baixo_custos/numS,2)
+    saude_meio_media = round(saude_meio_custos/numS,2)
+    saude_alto_media = round(saude_alto_custos/numS,2)
     
     print("Custo médio: " + str(mediaCustos))
 
-    print("\nCusto mínimo (setor financeiro): " + str(financeiro_min))
-    print("Custo médio (setor financeiro): " + str(financeiro_media))
-    print("Custo máximo (setor financeiro): " + str(financeiro_max))
-    print("\nCusto mínimo (setor de comércio): " + str(comercio_min))
-    print("Custo médio (setor de comércio): " + str(comercio_media))
-    print("Custo máximo (setor de comércio): " + str(comercio_max))
-    print("\nCusto mínimo (setor de saúde): " + str(saude_min))
-    print("Custo médio (setor de saúde): " + str(saude_media))
-    print("Custo máximo (setor de saúde): " + str(saude_max))
+    print("\nCusto (baixo) mínimo (setor financeiro): " + str(financeiro_baixo_min))
+    print("Custo (baixo) médio (setor financeiro): " + str(financeiro_baixo_media))
+    print("Custo (baixo) máximo (setor financeiro): " + str(financeiro_baixo_max))
+    print("Custo (meio) mínimo (setor financeiro): " + str(financeiro_meio_min))
+    print("Custo (meio) médio (setor financeiro): " + str(financeiro_meio_media))
+    print("Custo (meio) máximo (setor financeiro): " + str(financeiro_meio_max))
+    print("Custo (alto) mínimo (setor financeiro): " + str(financeiro_alto_min))
+    print("Custo (alto) médio (setor financeiro): " + str(financeiro_alto_media))
+    print("Custo (alto) máximo (setor financeiro): " + str(financeiro_alto_max))
 
-    print("Custo mínimo (malware): " + str(malware_min))
-    print("Custo médio (malware): " + str(malware_media))
-    print("Custo máximo (malware): " + str(malware_max))
-    print("\nCusto mínimo (phishing): " + str(phishing_min))
-    print("Custo médio (phishing): " + str(phishing_media))
-    print("Custo máximo (phishing): " + str(phishing_max))
-    print("\nCusto mínimo (DDoS): " + str(ddos_min))
-    print("Custo médio (DDoS): " + str(ddos_media))
-    print("Custo máximo (DDoS): " + str(ddos_max))
-    
+    print("\nCusto (baixo) mínimo (setor de comércio): " + str(comercio_baixo_min))
+    print("Custo (baixo) médio (setor de comércio): " + str(comercio_baixo_media))
+    print("Custo (baixo) máximo (setor de comércio): " + str(comercio_baixo_max))
+    print("Custo (meio) mínimo (setor de comércio): " + str(comercio_meio_min))
+    print("Custo (meio) médio (setor de comércio): " + str(comercio_meio_media))
+    print("Custo (meio) máximo (setor de comércio): " + str(comercio_meio_max))
+    print("Custo (alto) mínimo (setor de comércio): " + str(comercio_alto_min))
+    print("Custo (alto) médio (setor de comércio): " + str(comercio_alto_media))
+    print("Custo (alto) máximo (setor de comércio): " + str(comercio_alto_max))
+
+    print("\nCusto (baixo) mínimo (setor de saúde): " + str(saude_baixo_min))
+    print("Custo (baixo) médio (setor de saúde): " + str(saude_baixo_media))
+    print("Custo (baixo) máximo (setor de saúde): " + str(saude_baixo_max))
+    print("Custo (meio) mínimo (setor de saúde): " + str(saude_meio_min))
+    print("Custo (meio) médio (setor de saúde): " + str(saude_meio_media))
+    print("Custo (meio) máximo (setor de saúde): " + str(saude_meio_max))
+    print("Custo (alto) mínimo (setor de saúde): " + str(saude_alto_min))
+    print("Custo (alto) médio (setor de saúde): " + str(saude_alto_media))
+    print("Custo (alto) máximo (setor de saúde): " + str(saude_alto_max))
+
+    print("\nCusto (baixo) mínimo (malware): " + str(malware_baixo_min))
+    print("Custo (baixo) médio (malware): " + str(malware_baixo_media))
+    print("Custo (baixo) máximo (malware): " + str(malware_baixo_max))
+    print("Custo (meio) mínimo (malware): " + str(malware_meio_min))
+    print("Custo (meio) médio (malware): " + str(malware_meio_media))
+    print("Custo (meio) máximo (malware): " + str(malware_meio_max))
+    print("Custo (alto) mínimo (malware): " + str(malware_alto_min))
+    print("Custo (alto) médio (malware): " + str(malware_alto_media))
+    print("Custo (alto) máximo (malware): " + str(malware_alto_max))
+
+    print("\nCusto (baixo) mínimo (phishing): " + str(phishing_baixo_min))
+    print("Custo (baixo) médio (phishing): " + str(phishing_baixo_media))
+    print("Custo (baixo) máximo (phishing): " + str(phishing_baixo_max))
+    print("Custo (meio) mínimo (phishing): " + str(phishing_meio_min))
+    print("Custo (meio) médio (phishing): " + str(phishing_meio_media))
+    print("Custo (meio) máximo (phishing): " + str(phishing_meio_max))
+    print("Custo (alto) mínimo (phishing): " + str(phishing_alto_min))
+    print("Custo (alto) médio (phishing): " + str(phishing_alto_media))
+    print("Custo (alto) máximo (phishing): " + str(phishing_alto_max))
+
+    print("\nCusto (baixo) mínimo (DDoS): " + str(ddos_baixo_min))
+    print("Custo (baixo) médio (DDoS): " + str(ddos_baixo_media))
+    print("Custo (baixo) máximo (DDoS): " + str(ddos_baixo_max))
+    print("Custo (meio) mínimo (DDoS): " + str(ddos_meio_min))
+    print("Custo (meio) médio (DDoS): " + str(ddos_meio_media))
+    print("Custo (meio) máximo (DDoS): " + str(ddos_meio_max))
+    print("Custo (alto) mínimo (DDoS): " + str(ddos_alto_min))
+    print("Custo (alto) médio (DDoS): " + str(ddos_alto_media))
+    print("Custo (alto) máximo (DDoS): " + str(ddos_alto_max))
+
     '''
     # PLOTAGEM DE GRÁFICOS, A PARTIR DOS DADOS FILTRADOS
     # Gráfico de Barras de Erros dos custos, por cada setor
